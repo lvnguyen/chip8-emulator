4c61c2cde5e917291acb3efe2a43e1b9bd1ec000
lvnguyen <lvnguyen@mit.edu>
2015-12-23 14:14:40 -0500

    Add resource files

---
+++ emulator.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import pyglet
+
+class Chip8():
+	def __init__():
+		pass
+
+	def run():
+		pass
+
+if __name__ == "__main__":
+	width = 640
+	height = 320
+	chip8 = Chip8(width, height)
+	chip8.run()
